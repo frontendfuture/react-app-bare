b1e75cd0b382e93c89309cb5240d418405ad043e
Muhammad Azamuddin <azamuddin@live.com>
2016-04-05 18:46:51 +0700

    export default RootApp
    so that hot loader works

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
-class RootApp extends Component {
+export class RootApp extends Component {
     render() {
         return (
             <div>
-                Helo
+                Helo world great
             </div>
         );
     }
